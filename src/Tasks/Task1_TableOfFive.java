b5915f772ff29a80b059c40e039aca86cb363bb4
poonam-oss <banasodepoonam@gmail.com>
2025-06-10 00:48:31 +0530

    Task 1 Table of five with Printf Statement

---
+++ src/Tasks/Task1_TableOfFive.java
@@ -0,0 +1,19 @@
+package Tasks;
+
+public class Task1_TableOfFive {
+    int num;
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.printf(" 1 * 1 =%d \n ",n*1);
+        System.out.printf(" 1 * 2 =%d \n", n*2);
+        System.out.printf(" 1 * 3 =%d \n", n*3);
+        System.out.printf(" 1 * 4 =%d \n", n*4);
+        System.out.printf(" 1 * 5 =%d \n", n*5);
+        System.out.printf(" 1 * 6 =%d \n", n*6);
+        System.out.printf(" 1 * 7 =%d \n", n*7);
+        System.out.printf(" 1 * 8 =%d \n", n*8);
+        System.out.printf(" 1 * 9 =%d \n", n*9);
+        System.out.printf(" 1 * 10 =%d \n", n*10);
+    }
+}
