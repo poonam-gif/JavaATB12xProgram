28059656ab13acdc38e65baca936f3bac97fa39d
poonam-oss <banasodepoonam@gmail.com>
2025-06-21 11:55:55 +0530

    Que 2 operators

---
+++ src/Assesments/FirstTest_21June/Q2_ArithematicOperators.java
@@ -0,0 +1,16 @@
+package Assesments.FirstTest_21June;
+
+public class Q2_ArithematicOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b= 5;
+
+        int add = a+b;
+        int sub = a-b;
+        int div = a/b;
+        int mul = a*b;
+        int mod = a%2;
+
+        System.out.println("Addition:"+add+" Subtraction: "+sub+" Multiplication:"+mul+" Division:"+div+" Modulus: "+mod);
+    }
+}
