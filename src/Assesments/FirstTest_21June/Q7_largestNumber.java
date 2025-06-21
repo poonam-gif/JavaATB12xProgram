c7c6bc3953f0083cb890f75a7b0e2523b6848ec4
poonam-oss <banasodepoonam@gmail.com>
2025-06-21 12:30:31 +0530

    largest num

---
+++ src/Assesments/FirstTest_21June/Q7_largestNumber.java
@@ -0,0 +1,32 @@
+package Assesments.FirstTest_21June;
+
+import java.util.Scanner;
+
+public class Q7_largestNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = scanner.nextInt();
+
+        if (a == b && b == c) {
+            System.out.println("All numbers are equal.");
+        } else if (a >= b && a >= c) {
+            System.out.println("The largest number is: " + a);
+        } else if (b >= a && b >= c) {
+            System.out.println("The largest number is: " + b);
+        } else {
+            System.out.println("The largest number is: " + c);
+        }
+
+        scanner.close();
+    }
+    }
+
