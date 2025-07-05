2affd09c9892d93136518a07111092088a7bbe4d
poonam-oss <banasodepoonam@gmail.com>
2025-07-05 10:33:49 +0530

    Q4

---
+++ src/Assesments/Test_5July_WrapperClass/Q4_ArrayException.java
@@ -0,0 +1,15 @@
+package Assesments.Test_5July_WrapperClass;
+
+public class Q4_ArrayException {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,3,4,5,6,7};
+
+        for(int i=1;i<=arr.length;i++){
+            System.out.println(arr[i]);
+
+        }
+
+    }
+}
